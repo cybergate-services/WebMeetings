86b7e81af21efb021f8a57f06d6921af53218920
dungeon2567 <31074903+dungeon2567@users.noreply.github.com>
2020-04-27 12:29:27 -0300

    children 4

--- Client/src/components/PeerView.vue
+++ Client/src/components/PeerView.vue
@@ -4,7 +4,7 @@
       <video ref="video" class="full-width" autoplay muted v-if="mediaStream" playsinline />
 
       <div class="username">
-        <q-spinner-rings class="q-mr-sm"  color="light-green" v-if="isActiveSpeaker" size="12px"/>
+        <q-spinner-rings class="q-mr-sm"  color="light-green" v-if="isActiveSpeaker" size="14px"/>
         {{peer.displayName}}
       </div>
 
